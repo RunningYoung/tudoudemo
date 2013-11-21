aa1a41538c18cb2dc0653dce36274e7d6c840732
ibokan <ibokan@9300.local>
2013-11-21 11:58:47 +0800

    Initial Commit

---
+++ 土豆网视频客户端/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  土豆网视频客户端
+//
+//  Created by ibokan on 13-11-21.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
